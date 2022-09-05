b7520480d801ddfa2df726e44627c8cd343b1b2a
Slava <d-color.38@mail.ru>
2022-09-06 00:33:11 +0800

    DZ_15

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("Этот день Выходной");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Не корректый ввод");
+  }
+  else Console.WriteLine("Этот день Рабочий");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
